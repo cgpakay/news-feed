35284df30edc33625a883cb1c1355fdbcd3705c2
Cahide Gunes Pakay <159434758+cgpakay@users.noreply.github.com>
2024-03-25 17:59:25 +0100

    initial layout

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import data from './components/hackerNews.json'
+
+function App() {
+
+
+  return (
+    <>
+<div>
+  {/* {data.hits.map(newsPost => <div>{newsPost.title}</div>)} */}
+</div>
+    </>
+  )
+}
+
+export default App
